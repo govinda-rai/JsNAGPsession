2e84b08425dab91bff88fc424a9593b53cc10e72
govinda-rai <130759525+govinda-rai@users.noreply.github.com>
2023-05-12 11:33:18 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-// Import stylesheets
-import './style.css';
+abc = 20;
+console.log(abc);
+var abc;
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+//new type of variables
+
+if (true) {
+  let a = 10; //block scope
+  var b = 20; //functional scope
+}
+
+//console.log(a); //error
+console.log(b); //no error
+ x=3;y=5; z=2;
+console.log(x>y&&x<z);
+console.log(x>y&x<z);
+console.log(3=='3');//checks only value
+console.log(3==='3');//checks value and type
